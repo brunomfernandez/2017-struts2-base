4fc1e64689f4bd1eb3402b758aa341a0cf0fedaa
brunomfernandez <fernandezbrunomatias@gmail.com>
2017-04-06 14:17:42 -0300

    Error al correr

--- src/utn/frd/action/PersonaAction.java
+++ src/utn/frd/action/PersonaAction.java
@@ -5,12 +5,54 @@ import db.PersistentManager;
 import java.util.List;
 import com.opensymphony.xwork2.ActionSupport;
 
-public class PersonaAction extends ActionSupport {
+public class PersonaAction extends ActionSupport{
 	
 	private String name;
 	private String age;
 	private String gender;
 	private List<Persona> personas;
+	private long id;
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public List<Persona> getPersonas() {
+		return personas;
+	}
+
+	public void setPersonas(List<Persona> personas) {
+		this.personas = personas;
+	}
+
 	
 	public String save(){
 		personas = PersistentManager.getInstance();
